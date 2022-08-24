dd1437ed57c4572dd731471d743ea81efba617ca
Paul Arnaudo <parnaudo@gmail.com>
2022-08-24 14:42:32 -0700

    Update redshift_maitenance.sql

--- sql_tools/redshift_maitenance.sql
+++ sql_tools/redshift_maitenance.sql
@@ -1,8 +1,27 @@
-SELECT  schema as table_schema,
-        "table" as table_name,
-        size as used_mb
-FROM svv_table_info d
-where table_schema = 'production_warehouse'
+select
+  cast(use2.usename as varchar(50)) as owner,
+  pgc.oid,
+  trim(pgdb.datname) as Database,
+  trim(pgn.nspname) as Schema,
+  trim(a.name) as Table,
+  b.gbytes,
+  a.rows
+from
+ (select db_id, id, name, sum(rows) as rows
+  from stv_tbl_perm a
+  group by db_id, id, name
+  ) as a
+ join pg_class as pgc on pgc.oid = a.id
+ left join pg_user use2 on (pgc.relowner = use2.usesysid)
+ join pg_namespace as pgn on pgn.oid = pgc.relnamespace
+    and pgn.nspowner > 1
+ join pg_database as pgdb on pgdb.oid = a.db_id
+ join
+   (select tbl, count(*)/1000 as gbytes
+    from stv_blocklist
+    group by tbl
+   ) b on a.id = b.tbl
+ order by gbytes desc, a.db_id, a.name;
 
 
 #Running queries
